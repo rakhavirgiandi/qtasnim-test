4c1f6e5646fe474ab2dd7a49ddac7b88846ba961
Rakha Ridwan Virgiandi <rakhardwan@gmail.com>
2024-12-11 20:06:53 +0700

    product resource and collection

---
+++ app/Http/Resources/ProductCollection.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'current_page' => $this->resource->currentPage(),
+                'from' => $this->resource->firstItem(),
+                'last_page' => $this->resource->lastPage(),
+                'per_page' => $this->resource->perPage(),
+                'to' => $this->resource->lastItem(),
+                'total' => $this->resource->total(),
+                'path' => $this->resource->path(),
+            ],
+        ];
+    }
+
+    // public function paginationInformation($request, $paginated, $default)
+    // {
+    //     unset($default['meta']['links']);
+ 
+    //     return $default;
+    // }
+}
---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'price' => $this->price,
+            'full_price' => $this->full_price,
+            'stock' => $this->stock,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+            'category' => [
+                'id' => $this->category_id,
+                'key' => $this->category_key,
+                'title' => $this->category_title,
+            ]
+        ];
+    }
+    
+}
