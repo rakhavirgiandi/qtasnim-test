3c39f29cbf729ef89a03138684cb5bbcc9e02ea6
Rakha Ridwan Virgiandi <rakhardwan@gmail.com>
2024-12-12 21:09:15 +0700

    add DataSeeder class to call seeder main function

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            UserSeeder::class
+            UserSeeder::class,
+            DataSeeder::class
         ]);
     }
 }
