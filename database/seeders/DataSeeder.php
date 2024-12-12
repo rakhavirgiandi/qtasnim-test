d0892bdb2df854f27f3c2e20c9e138fb96f136e5
Rakha Ridwan Virgiandi <rakhardwan@gmail.com>
2024-12-13 00:52:28 +0700

    optimizing data seeder

--- database/seeders/DataSeeder.php
+++ database/seeders/DataSeeder.php
@@ -153,45 +153,29 @@ class DataSeeder extends Seeder
                 if ($category) {
                     $product->category()->associate($category);
                     $product->save();
-                    $amount_reduction_history = [];
-                    // $has_product_id = $product->id;
-
-                    foreach ($orders as $order_key => $order_value) {
-                        if ($order_value['product_key'] == $value['key']) {
-                            var_dump($order_value);
-                            $find_amount_reduction_history = Arr::first($amount_reduction_history, function ($find_amount_reduction_history_value, $find_amount_reduction_history_key) use ($order_value) {
-                                return $find_amount_reduction_history_value['product_key'] === $order_value['product_key'];
-                            });
-                            
-                            $previous_stock = $product->stock;
-
-                            if ($find_amount_reduction_history) {
-                                array_map(function ($amount_reduction_history_value) use ($order_value) {
-                                    if ($amount_reduction_history_value['product_key'] === $order_value['product_key']) {
-                                        $amount_reduction_history_value['amount_value'] += $order_value['order_amount'];
-                                    }
-                                    return $amount_reduction_history_value;
-                                }, $amount_reduction_history);
-
-                                $previous_stock = $previous_stock - $find_amount_reduction_history['amount_value'];
-                            } else {
-                                $amount_reduction_history[count($amount_reduction_history) === 0 ? count($amount_reduction_history) : count($amount_reduction_history) + 1] = [
-                                    "product_key" => $order_value['product_key'],
-                                    "amount_value" => $order_value['order_amount']
-                                ];
-                            }
-                            
-                            $order = new Order;
-                            $order->previous_stock = $previous_stock;
-                            $order->order_amount = $order_value['order_amount'];
-                            $order->order_date = $order_value['order_date'];
-                            $order->product()->associate($product);
-                            
-                            $order->save();
-                        }
-                    }
 
                 }
+            }
+
+            foreach ($orders as $order_key => $order_value) {
+                $product_key = $order_value['product_key'];
+                $product = array_values(array_filter($products, function ($val) use ($product_key) {
+                    return $val['key'] === $product_key;
+                }))[0] ?? null;
+
+                $find_product = Product::where('name', '=', $product['name']);
+                $get_product = $find_product->first();
+                    
+                $order = new Order;
+                $order->previous_stock = $get_product->stock;
+                $order->order_amount = $order_value['order_amount'];
+                $order->order_date = $order_value['order_date'];
+                $order->product()->associate($get_product);
+                
+                $final_stock = $get_product->stock - $order_value['order_amount'];
+
+                $get_product->update(['stock' => $final_stock]);
+                $order->save();
             }
             
             DB::commit();
